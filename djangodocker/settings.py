a5c0c121ffeadd6a411b9008f58eae078c2c25dd
Ahsan10426 <77637759+Ahsan10426@users.noreply.github.com>
2023-04-06 08:55:38 +0500

    Update settings.py

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'https://django-ahsan10426.cloud.okteto.net',
 ]
 
 ROOT_URLCONF = 'djangodocker.urls'
